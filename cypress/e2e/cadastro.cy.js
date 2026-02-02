2fcc7d02aff406d74b4473530f859f0d2a2e1eb1
Willian Sieben <willsieben4587@gmail.com>
2026-02-02 16:40:04 -0300

    automação da tela de catalago

---
+++ cypress/e2e/cadastro.cy.js
@@ -0,0 +1,28 @@
+/// <reference types="cypress"/>
+import  {  faker  }  from  '@faker-js/faker' ;
+
+describe('Funcionalidade: Cadastro de hub de leitura', () => {
+
+    beforeEach(() => {
+        cy.visit('register.html')
+    });
+
+    it('Deve preencher o formulário de cadastro com sucesso', () => {
+        let name = faker.person.fullName()
+        let email = faker.internet.email()
+        let phone = faker.phone.number()
+        cy.get('#name').type(name)
+        cy.get('#email').type(email)
+        cy.get('#phone').type(phone)
+        cy.get('#password').type('SenhaSegura123!')
+        cy.get('#confirm-password').type('SenhaSegura123!')
+        cy.get('#terms-agreement').check()
+        cy.get('#register-btn').click()
+        //deve exibir mensagem de sucesso
+        cy.url().should('include', 'dashboard')
+        cy.get('#user-name').should('contain', name)
+        
+
+    });
+
+});
